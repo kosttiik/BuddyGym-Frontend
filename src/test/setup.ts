3e3a77779a08d7d23f2f25e8f0eb9a9380ea2b29
kosttiik <k.samoylove@mail.ru>
2026-07-18 12:26:42 +0300

    fix onboarding swipe: real pixel drag instead of percent/px constraint mismatch

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,5 +1,14 @@
 import "@testing-library/jest-dom/vitest";
 
+if (typeof ResizeObserver === "undefined") {
+  class ResizeObserverStub {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  }
+  globalThis.ResizeObserver = ResizeObserverStub;
+}
+
 beforeEach(() => {
   try {
     window.localStorage.setItem("bg.seenOnboarding", "1");
